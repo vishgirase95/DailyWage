15b62747cdf96bc32bb636a645db6de44cec928d
vish <vishgirase95@gmail.com>
2021-10-20 22:22:08 +0530

    [vishal] refator reverse to Monthly wage from max hr

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,14 +10,12 @@ namespace ConsoleApp1
             int ratePerHr = 20;
             int empHr = 0;
             
-            int Workingday = 30;
+            int Workingday = 20;
             int PartTime = 0;
             int FullTime = 0;
             int Absent = 0;
             int TotalWorkingHr = 0;
-            int nTotalWorkingHr = 0;
-            int i = 1;
-            while( (i <= Workingday) && (nTotalWorkingHr<=100)) {
+            for (int i = 1; i <= Workingday; i++) {
                 Random num = new Random();
                 int numGen = num.Next(0, 3);
                 switch (numGen)
@@ -25,28 +23,23 @@ namespace ConsoleApp1
                     case 0:
                         empHr = 8;
                         FullTime++;
-                        TotalWorkingHr = TotalWorkingHr + empHr;
+                        
                         break;
                     case 1:
                         empHr = 4;
                         PartTime++;
-                        TotalWorkingHr = TotalWorkingHr + empHr;
-
                         break;
                     default:
                         empHr = 0;
-                        TotalWorkingHr = TotalWorkingHr + empHr;
-
                         Absent++;
                         break;
 
                             
                 }
-                nTotalWorkingHr = TotalWorkingHr;
-                i++;
+              
             }
 
-            Console.WriteLine(TotalWorkingHr);
+
             Console.WriteLine("Has Worked Full Time: "+FullTime+"   Has Worked Part Time: "+PartTime+  "  Was Absent this days: "+Absent);
 
             int MonthlyWage = ((FullTime*8)+(PartTime*4)+(Absent*0))*ratePerHr;

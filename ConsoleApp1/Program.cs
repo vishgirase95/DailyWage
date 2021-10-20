a2762456ba2dd6f92fa580090d92ba1b8c0b97ed
vish <vishgirase95@gmail.com>
2021-10-20 15:08:00 +0530

    [vishal] addd empAttendance

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random num = new Random();
+            int numGen = num.Next(0,2);
+            if (numGen == 1)
+            {
+                Console.WriteLine("Emp is present");
+            }
+            else
+            {
+                Console.WriteLine("Emp is absent");
+            }
+        }
+    }
+}

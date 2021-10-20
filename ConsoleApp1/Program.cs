cacd8efd71783b035c2660e454a8bf9f38e59d14
vish <vishgirase95@gmail.com>
2021-10-21 00:21:51 +0530

    [vishal] refator to class method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,20 +4,23 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static int empHr = 0;
+        public static int Workingday = 30;
+        public static int ratePerHr = 20;
+        public static int PartTime = 0;
+
+        public static int FullTime = 0;
+        public static int Absent = 0;
+        public static int TotalWorkingHr = 0;
+        public static int nTotalWorkingHr = 0;
+        public static int i = 1;
+
+        public static int EmpWage()
         {
-            
-            int ratePerHr = 20;
-            int empHr = 0;
-            
-            int Workingday = 30;
-            int PartTime = 0;
-            int FullTime = 0;
-            int Absent = 0;
-            int TotalWorkingHr = 0;
-            int nTotalWorkingHr = 0;
-            int i = 1;
-            while( (i <= Workingday) && (nTotalWorkingHr<=100)) {
+
+            while ((i <= Workingday) && (nTotalWorkingHr <= 100))
+            {
                 Random num = new Random();
                 int numGen = num.Next(0, 3);
                 switch (numGen)
@@ -40,17 +43,22 @@ namespace ConsoleApp1
                         Absent++;
                         break;
 
-                            
+
                 }
                 nTotalWorkingHr = TotalWorkingHr;
                 i++;
             }
 
             Console.WriteLine(TotalWorkingHr);
-            Console.WriteLine("Has Worked Full Time: "+FullTime+"   Has Worked Part Time: "+PartTime+  "  Was Absent this days: "+Absent);
+            Console.WriteLine("Has Worked Full Time: " + FullTime + "   Has Worked Part Time: " + PartTime + "  Was Absent this days: " + Absent);
 
-            int MonthlyWage = ((FullTime*8)+(PartTime*4)+(Absent*0))*ratePerHr;
-            Console.WriteLine("The Monthly wage of month is..  " + MonthlyWage);
+            int MonthlyWage = ((FullTime * 8) + (PartTime * 4) + (Absent * 0)) * ratePerHr;
+            Console.WriteLine("The monthly wage will be "+ MonthlyWage);
+            return MonthlyWage;
+        }
+        static void Main(string[] args)
+        {
+           EmpWage();
         }
     }
 }
